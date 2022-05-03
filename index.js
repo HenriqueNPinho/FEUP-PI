78110f7c6961d4e474e229f4f21a4ab6b687fd21
up201904529 <up201904529@fe.up.pt>
2022-05-03 13:58:56 +0100

    asdf

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ const fs = require('fs');
 const server = http.createServer(function (request, response) {
   const parsedUrl = url.parse(request.url,true);
   switch(parsedUrl.pathname) {
-    case '/leic_gui':
+    case '/load':
     if(request.method == "POST"){
-      rquestRanking(request, response);
+      requestRanking(request, response);
     }
     break;
   }
 });
 
 
-function rquestRanking(request,response){
-  fs.readFile('ranking.json',function(err,data) {
+function requestRanking(request,response){
+  fs.readFile('data1.json',function(err,data) {
     if(! err) {
       dados = JSON.parse(data.toString());
       response.writeHead(200, {'Content-Type': 'application/javascript','Cache-Control': 'no-cache','Access-Control-Allow-Origin': '*'})
-      response.write(JSON.stringify(data));
+      response.write(JSON.stringify(dados));
       response.end();
     }
   });
 }
 
-server.listen(8141);
+server.listen(5000);
