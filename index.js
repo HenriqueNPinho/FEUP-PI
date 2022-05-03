b3e65a3570a9a437b5723c63d3f0c3dac6abb737
Henrique <up201805000@up.pt>
2022-05-04 00:24:04 +0100

    af

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const http = require('http');
 const fs = require('fs');
 const port = 8000
-var json = require('./data.json'); //(with path)
-var obj = JSON.parse(json);
+//(with path)
 
 
 var server = http.createServer(function(request, response) {
-  console.log(obj)
+  
   fs.readFile('./index.html', function(err, html) {
     response.writeHeader(200, {"Content-Type": "text/html"});  
     response.write(html);
--- script.js
+++ script.js
@@ -1,54 +1,16 @@
-var myserver = 'http://localhost:5000/';
+var tables=["mesa1", "mesa2", "mesa3", "mesa4", "mesa5"]
+var colors=["green", "yellow", "yellow", "green", "red"]
 
-function start() {
-    setInterval(fds, 5000);
-  }
+function changeColor(tables, colors) {
+    // your code here
+    function leftLight() {
 
-function fds() {
-    let object = {};
-    serveRequest('load', object);
-}
+      for(let i = 0; i<tables.length;i++){
+        var element = document.getElementById(tables[i]);
+        element.style.color = colors[i];
+      }
 
-function serveRequest(command, object) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', myserver.concat(command), true); 
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      const data = JSON.parse(xhr.responseText);
-      switch(command) {
-        case 'load':
-            console.log("NIGGER");
-        if (data.error) {
-          alert(JSON.stringify(data.error));
-        } else {
-          load(data.leic);
-        }
-        break;
     }
-  }
-}
-  xhr.send(JSON.stringify(object));
-}
-
-function load(data){
-    var table = document.getElementById('load');
-    let result = data.reduce((acc,cur) =>
-    acc.push(Object.values(cur)) && acc, [Object.keys(data[0])])
-
-    var html = '<table class="pont">';
-    html += '<tr">';
-    html += '<th>' + "mesa" + '</th>';
-    html += '<th>' + "sensor" + '</th>';
-    html += '<th>' + "name" + '</th>';
-    html += '</tr>';
-
-    for( var i = 1; i < result.length; i++) {
-        html += '<tr>';
-        for( var j in result[i] ) {
-            html += '<td>' + result[i][j] + '</td>';
-        }
-        html += '</tr>';
-    }
-    html += '</table>';
-    table.innerHTML = html;
-  }
+    // Also don't forget to call the function
+    leftLight();
+} 
