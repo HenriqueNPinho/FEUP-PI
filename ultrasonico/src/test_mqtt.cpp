38324050fc2cd105ecc1a1b6ba31fddb955b5132
up201904529 <up201904529@fe.up.pt>
2022-06-11 19:34:19 +0100

    test mqtt broker

---
+++ ultrasonico/src/test_mqtt.cpp
@@ -0,0 +1,63 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char *ssid = "Vodafone-92685F";
+const char *pass = "metro296";
+
+const char *broker = "broker.mqttdashboard.com";
+const int port = 8000;
+
+WiFiClient espClient;
+PubSubClient client(broker, port, espClient);
+
+void setupWifi()
+{
+    delay(100);
+    Serial.print("\nConnecting to");
+    Serial.println(ssid);
+    WiFi.begin(ssid, pass);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(100);
+        Serial.print("-");
+    }
+    Serial.print("\nConnected to");
+    Serial.println(ssid)
+}
+
+void reconnect()
+{
+    while (!client.connected())
+    {
+        Serial.print("\nConnecting to ");
+        Serial.println(broker);
+        if (client.connect())
+        {
+            Serial.print("\nConnected to ");
+            Serial.println(broker);
+        }
+        else
+        {
+            Serial.println("\nTrying connect again");
+            delay(5000);
+        }
+    }
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    setupWifi();
+    client.setServer(broker, port);
+}
+
+void loop()
+{
+    // put your main code here,to run repeatedly:
+    if (!client.connected())
+    {
+        reconnect();
+    }
+    client.loop();
+    client.publish("secretaria", '{ "mesa": "2", "sensor": "Livre" }');
+}
