64cdb521e1b75a8fa59246e14930760ab46ae98f
Henrique <up201805000@up.pt>
2022-07-12 13:38:15 +0100

    update

--- dashboard/js/index.js
+++ dashboard/js/index.js
@@ -8,6 +8,24 @@ function refreshTime() {
 
 setInterval(refreshTime, 1000);
 
+
+function switchNext(table) {
+  
+  var element = document.getElementById("color" + table)
+  console.log(element.className)
+  if(element.className == "estado_green") {
+    renderLeic(`{ "desk": "${table}", "sensor": "Unavailable" }`)
+  }
+
+  if(element.className == "estado_red") {
+    renderLeic(`{ "desk": "${table}", "sensor": "Away" }`)
+  }
+
+  if(element.className == "estado_yellow") {
+    renderLeic(`{ "desk": "${table}", "sensor": "Free" }`)
+  }
+}
+
 function renderLeic(message) {
   secretaria = JSON.parse(message);
   console.log(secretaria);
@@ -19,11 +37,11 @@ function renderLeic(message) {
   if(secretaria.sensor == "Away") sensorPT = "Ausente";
 
   if (secretaria.sensor == "Free") {
-    template += `<p class="estado_green"> <i class="fa-solid fa-circle-check fa-bounce" style="font-size:30px;color: #34ace0;"></i>${sensorPT} </p>`;
+    template += `<p id= "color${secretaria.desk}" class="estado_green"> <i class="fa-solid fa-circle-check fa-bounce" style="font-size:30px;color: #34ace0;"></i>${sensorPT} </p>`;
   } else if (secretaria.sensor == "Unavailable") {
-    template += `<p class="estado_red"> <i class="fa-solid fa-hourglass fa-spin" style="font-size:30px;color: #2c2c54;margin-right: 10px"></i> ${sensorPT} </p>`;
+    template += `<p id= "color${secretaria.desk}" class="estado_red"> <i class="fa-solid fa-hourglass fa-spin" style="font-size:30px;color: #2c2c54;margin-right: 10px"></i> ${sensorPT} </p>`;
   } else if (secretaria.sensor == "Away") {
-    template += `<p class="estado_yellow"> <i class="fa-solid fa-hourglass fa-spin" style="font-size:30px;color: #2c2c54;margin-right: 10px"></i> ${sensorPT} </p>`;
+    template += `<p id= "color${secretaria.desk}" class="estado_yellow"> <i class="fa-solid fa-hourglass fa-spin" style="font-size:30px;color: #2c2c54;margin-right: 10px"></i> ${sensorPT} </p>`;
   }
 
   var element = "led"+secretaria.desk;
