8ef419fe8c8a3bfa84376959292ed400ade88efd
santos <SoloQueueTrash>
2022-07-26 10:19:37 +0100

    html button update (mqtt publish)

--- dashboard/js/subscribe.js
+++ dashboard/js/subscribe.js
@@ -5,6 +5,31 @@ const clientID = "web" + new Date().getTime();
 
 window.onload = sub_mqtt_msg;
 
+function send_mqtt_msg(table) {
+  var element = document.getElementById("color" + table);
+    console.log(element.className);
+    var mqtt_msg = "";
+    if (element.className == "estado_green") {
+      mqtt_msg = `{ "desk": "${table}", "sensor": "Unavailable" }`;
+      renderLeic(mqtt_msg);
+    }
+
+    if (element.className == "estado_red") {
+      mqtt_msg = `{ "desk": "${table}", "sensor": "Away" }`;
+      renderLeic(mqtt_msg);
+    }
+
+    if (element.className == "estado_yellow") {
+      mqtt_msg = `{ "desk": "${table}", "sensor": "Free" }`;
+      renderLeic(mqtt_msg);
+    }
+    console.log(mqtt_msg);
+
+    message = new Paho.MQTT.Message(mqtt_msg);
+    message.destinationName = mqtt_destname;
+    client.send(message);
+}
+
 function sub_mqtt_msg() {
   client = new Paho.MQTT.Client(mqtt_server, mqtt_port, clientID);
   console.log(clientID);
